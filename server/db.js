6092d30106c9691d4f948529fc158fff23d69530
Daniel Thapa <dannymagar37@gmail.com>
2021-03-25 22:38:54 +0545

    database connection

---
+++ server/db.js
@@ -0,0 +1,11 @@
+import { password, database, user, host } from "./secret";
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: user,
+  password: password,
+  host: host,
+  port: port,
+  database: database,
+});
+
+module.exports = pool;
